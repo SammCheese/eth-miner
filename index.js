4c01971fa96986221fd095b71be566eeca1787dd
SammCheese <chara.notfrisk@gmail.com>
2021-11-11 11:47:02 +0100

    Notice when TRex is Missing

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const { Plugin } = require('powercord/entities');
+const { existsSync, mkdirSync } = require('fs');
 
 const Settings = require('./components/settings');
 const f = require('./components/Functions');
 
 module.exports = class ethMiner extends Plugin {
-  async startPlugin() {
+  startPlugin() {
+    if (!existsSync('./TRex')) {
+      mkdirSync('./TRex');
+      powercord.api.notices.sendToast('eth-miner', {
+        header: 'Component Missing!',
+        content: 'We couldnt find TRex, an important component, Please download it from here https://github.com/trexminer/T-Rex/releases/ and place the t-rex.exe into the eth-miner/TRex Folder',
+        type: 'danger',
+        buttons: [
+          {
+            text: 'Got it',
+            look: 'ghost'
+          }
+        ]
+      });
+    }
     powercord.api.settings.registerSettings(this.entityID, {
       label: 'Ethereum Miner',
       category: this.entityID,
